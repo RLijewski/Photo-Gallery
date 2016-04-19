b100aac27badf9b4ebe476f472fa33d3fb4770ed
Ryan Lijewski <RLijewski@users.noreply.github.com>
2016-04-19 18:31:30 -0500

    Added list.js
    Added list.js to filter photos on search. Formatted html to list.js
    needs. Small CSS tweets.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+//rebox
 $('.photo-gallery').rebox({ selector: 'a' });
+
+
+//list
+var options = {
+    valueNames: [ { attr: 'title', name: 'photo-caption'} ]
+};
+
+var hackerList = new List('photos', options);
